ec9fbc0106d786be0ea4d748423179d0d6d7feae
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-21 17:59:23 -0300

    feat: create database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://otavio:MyPassword@cluster0.gv39ixp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
