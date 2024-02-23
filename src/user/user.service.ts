285f7d1ca8a58ede9e112f95d2c959fe11439358
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-22 22:55:21 -0300

    feat: create function to find user by name and add handle error when user name has already in use

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -9,14 +9,31 @@ import { User, UserDocument } from './schemas/user.schema';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async create(createUserDto: CreateUserDto): Promise<UserDto> {
-    const createdUser = new this.userModel(createUserDto);
-    const savedUser = await createdUser.save();
-    return savedUser.toObject();
-  }
-
   async findAll(): Promise<UserDto[]> {
     const users = await this.userModel.find().select('id nome').exec();
     return users.map((user) => user.toObject());
+  }
+
+  async findByName(nome: string): Promise<UserDto[]> {
+    const users = await this.userModel
+      .find({ nome: nome })
+      .select('id nome')
+      .exec();
+    return users.map((user) => user.toObject());
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<UserDto> {
+    const { nome } = createUserDto;
+
+    // Verificar se já existe um usuário com o mesmo nome
+    const existingUser = await this.userModel.findOne({ nome }).exec();
+    if (existingUser) {
+      throw new ConflictException('Já existe um usuário com este nome.');
+    }
+
+    // Se não houver nenhum usuário com o mesmo nome, criar e salvar o novo usuário
+    const createdUser = new this.userModel(createUserDto);
+    const savedUser = await createdUser.save();
+    return savedUser.toObject();
   }
 }
