af5f52010f631b4fb39a39730f213395f641c317
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-22 22:13:39 -0300

    fix: update imports and her functions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
@@ -9,7 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       'mongodb+srv://otavio:MyPassword@cluster0.gv39ixp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     ),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
