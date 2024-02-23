d66ed26c3d89264400e621f13c7f3bf30c0936b2
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-22 22:52:52 -0300

    feat: referencing module in the import section

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://otavio:MyPassword@cluster0.gv39ixp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     ),
+    UserModule,
   ],
   controllers: [],
   providers: [],
