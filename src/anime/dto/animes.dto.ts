2e95ae365cc5f8a5e6b99536555a6c119f44cb0e
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-03-03 14:13:33 -0300

    feat: create anime dto file

---
+++ src/anime/dto/animes.dto.ts
@@ -0,0 +1,10 @@
+export class AnimeDto {
+  userId: number;
+  title: string;
+  photo: string;
+  episodesWatched: number;
+  progress: string;
+  dayOfWeek: string;
+  lastDayWatched: string;
+  season: string;
+}
