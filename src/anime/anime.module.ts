1b541d78a3a04e7a561c14d638b2bb1b40e244d8
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-03-03 14:14:57 -0300

    feat: create anime module file

---
+++ src/anime/anime.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Anime, AnimeSchema } from './schemas/anime.schema';
+import { AnimeController } from './anime.controller';
+import { AnimeService } from './anime.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Anime.name, schema: AnimeSchema }]),
+  ],
+  controllers: [AnimeController],
+  providers: [AnimeService],
+})
+export class AnimeModule {}
