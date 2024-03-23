511f83c8bef2d9c7b12550afa34681f4a690e98d
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-03-23 20:43:17 -0300

    update: get user and create user response possibility

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,20 +2,25 @@ import { Controller, Get, Post, Body, Query } from '@nestjs/common';
 import { UsersService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserDto } from './dto/user.dto';
+import { ConflictResponse } from 'src/interfaces/response';
 
 @Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  async findAll(@Query('nome') name?: string): Promise<UserDto[]> {
+  async findAll(
+    @Query('nome') name?: string
+  ): Promise<UserDto[] | ConflictResponse> {
     return name
       ? this.usersService.findByName(name)
       : this.usersService.findAll();
   }
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto): Promise<UserDto> {
+  async create(
+    @Body() createUserDto: CreateUserDto
+  ): Promise<UserDto | ConflictResponse> {
     return this.usersService.create(createUserDto);
   }
 }
