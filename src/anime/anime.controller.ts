751501aa8890e17944fb92c16dee9bebabc346d8
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-03-27 16:09:28 -0300

    update: function response type and import conflict interface

--- src/anime/anime.controller.ts
+++ src/anime/anime.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AnimeDto } from './dto/animes.dto';
 import { CreateAnimeDto } from './dto/create-anime.dto';
 import { AnimeService } from './anime.service';
+import { ConflictResponse } from 'src/interfaces/response';
 
 @Controller('anime')
 export class AnimeController {
   constructor(private readonly animeService: AnimeService) {}
 
   @Get()
-  async findAll(): Promise<AnimeDto[]> {
+  async findAll(): Promise<AnimeDto[] | ConflictResponse> {
     return this.animeService.findAll();
   }
 
