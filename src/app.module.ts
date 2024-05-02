0900420fa1f0784e1ea9e633ca591bd5befe4210
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-05-02 17:09:40 -0300

    fix: best practices with environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,7 @@ import { AnimeModule } from './anime/anime.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://otavio:12345@cluster0.gv39ixp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    ),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
     UserModule,
     AnimeModule,
   ],
