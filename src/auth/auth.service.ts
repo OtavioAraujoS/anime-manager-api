1440ebc3b16504c258392444a24b91d259459f0e
OtavioAraujoS <otavioaraujo490@gmail.com>
2025-11-04 09:10:35 -0300

    feat: implement AuthService for user authentication and registration

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from 'src/user/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findByName(username);
+    if (user && (await bcrypt.compare(pass, user?.[0]?.password || ''))) {
+      const { ...result } = user[0];
+      return result;
+    }
+    throw new UnauthorizedException('Usuário ou senha inválidos');
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async register(username: string, password: string) {
+    return this.usersService.create({ name: username, password });
+  }
+}
