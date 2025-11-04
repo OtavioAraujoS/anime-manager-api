617bf9960b223aee13479bce006c8d96cc573bdc
OtavioAraujoS <otavioaraujo490@gmail.com>
2025-11-04 10:10:17 -0300

    feat: enhance AuthService with error handling and response types

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { LogLevel, logMessage } from 'src/utils/logMessage';
 import { UsersService } from 'src/user/user.service';
+import { ConflictResponse } from 'src/interfaces/response';
+import { UserDto } from 'src/user/dto/user.dto';
+import { LoginDTO } from './dto/login-dto';
 
 @Injectable()
 export class AuthService {
@@ -10,23 +14,79 @@ export class AuthService {
     private jwtService: JwtService
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findByName(username);
-    if (user && (await bcrypt.compare(pass, user?.[0]?.password || ''))) {
-      const { ...result } = user[0];
-      return result;
+  async validateUser(
+    username: string,
+    pass: string
+  ): Promise<UserDto | ConflictResponse> {
+    try {
+      if (!username || !pass) {
+        const response: ConflictResponse = {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: 'Usuário inválido.',
+        };
+        return response;
+      }
+      const user = await this.usersService.findByName(username);
+      if (user && (await bcrypt.compare(pass, user?.[0]?.password || ''))) {
+        const { ...result } = user[0];
+        return result;
+      }
+      throw new UnauthorizedException('Usuário ou senha inválidos');
+    } catch (error) {
+      logMessage(error.message, LogLevel.ERROR);
+      const response: ConflictResponse = {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: error.message,
+      };
+
+      return response;
     }
-    throw new UnauthorizedException('Usuário ou senha inválidos');
   }
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.id };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+  async login(user: UserDto): Promise<LoginDTO | ConflictResponse> {
+    try {
+      if (!user.name || !user.password) {
+        const response: ConflictResponse = {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: 'Usuário inválido.',
+        };
+        return response;
+      }
+      const payload = { username: user.name, sub: user.id };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    } catch (error) {
+      logMessage(error.message, LogLevel.ERROR);
+
+      const response: ConflictResponse = {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: error.message,
+      };
+
+      return response;
+    }
   }
 
-  async register(username: string, password: string) {
-    return this.usersService.create({ name: username, password });
+  async register(
+    username: string,
+    password: string
+  ): Promise<UserDto | ConflictResponse> {
+    try {
+      if (!password || !username) {
+        logMessage('Usuário ou senha são obrigatórios', LogLevel.ERROR);
+        throw new UnauthorizedException('Usuário ou senha são obrigatórios');
+      }
+      return this.usersService.create({ name: username, password });
+    } catch (error) {
+      logMessage(error.message, LogLevel.ERROR);
+
+      const response: ConflictResponse = {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: error.message,
+      };
+
+      return response;
+    }
   }
 }
